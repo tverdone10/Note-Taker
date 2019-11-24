faccf3f0e36839d77fc6dae1dfbb0658004866e6
tverdone <tverdone10@gmail.com>
2019-11-24 15:30:33 -0800

    trying to get apiroutes working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,34 @@
 var express = require('express')
 var db = require('../db/db.json')
+var router=require('express').Router()
+var fs=require('fs')
+// var db = require("../db/db.json")
+
+router.get("/notes", function (req, res){
+        return res.json(db)
+        
+});
+
+router.post("/notes", function (req, res){
+
+    return res.json(fs.writeFile(db, "utf8", (err) => {
+           if (err) throw err;
+           console.log("Sucess")
+       }))
+
+})
+
+
+// router.delete("/notes", function (req, res){
+//     return res.json(db)
+// })
+
+
+// router.post("/notes", function (req, res){
+    
+    // })
+module.exports = router 
+        // return res.json(fs.writeFile("db.json" , "utf8", (err) => {
+        //     if (err) throw err;
+        //     console.log("Sucess");
+        //     })
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express=require('express')
 var fs = require('fs')
 const htmlRoutes = require("./routes/htmlRoutes")
-
+var apiRoutes = require("./routes/apiRoutes")
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// Front-end Routes
 app.use("/", htmlRoutes)
-// require("./routes/htmlRoutes")(app)
-
+// Back-end Routes
+app.use("/api", apiRoutes)
 app.listen(port, () => {
     console.log("We're listening to HOT " + port + "FM" )
 })
